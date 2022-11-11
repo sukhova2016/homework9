24ff00c7a8ef0b820fcb1eaddb44b9646cbc6ea6
Polina Drokina <sukhova2016@inbox.ru>
2022-11-11 16:17:24 +0400

    Добавлено решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите натуральное число больше 1: ");
+int n = Convert.ToInt16(Console.ReadLine());
+
+void NumberCounter(int n)
+{
+    if (n < 0) Console.Write($"{n} не натуральное число");
+    if (n == 0) return;
+    Console.Write("{0,4}",n);
+    NumberCounter (n-1);
+}
+NumberCounter(n);
