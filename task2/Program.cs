b3f056f4bc37c62f97e1f13fa486001a914e2b4b
Polina Drokina <sukhova2016@inbox.ru>
2022-11-11 17:02:05 +0400

    Добавлено решение второй задачи

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿int m = InputInt("Введите натуральное число M:");
+int n = InputInt("Введите натуральное число N:");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+    return n;
+    return n + CountNaturalSum(m, n - 1);
+}
